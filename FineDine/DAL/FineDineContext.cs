e5aa8cb4a22fde05d441882cb15fe1773efbfef8
Marijan Barišin <barisin.marijan@gmail.com>
2015-12-01 15:27:19 +0100

    Revert "Scaffolding done"
    This reverts commit 5a90085045bb306517b265bd117553ba5eb59b4f.

--- FineDine/DAL/FineDineContext.cs
+++ FineDine/DAL/FineDineContext.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using FineDine.Models;
 
 namespace FineDine.DAL
 {
-    public class FineDineContext : DbContext
+    public class FineDineContext
     {
-        public DbSet<BusinessUser> BusinessUsers { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<Establishment> Establishments { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<PersonalUser> PersonalUsers { get; set; }
-        public DbSet<Tag> Tags { get; set; }
     }
 }
