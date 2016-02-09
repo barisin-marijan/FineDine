e3bd525b787070cc4816788233ca39b872fab341
Marijan Barišin <barisin.marijan@gmail.com>
2016-02-09 15:37:30 +0100

    First Ang2 steps (establishment-details tag created and adde to Establishment/Details).

--- FineDine/Scripts/App/app.js
+++ FineDine/Scripts/App/app.js
@@ -1,1 +1,36 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("angular2/core");
+var browser_1 = require('angular2/platform/browser');
+//import {TodoItem} from "./Model/TodoItem"
+//import {TodoInput} from "./Components/TodoInput"
+//import {TodoProgress} from "./Components/TodoProgress"
+//import {TodoList} from "./Components/TodoList"
+//import {TodoService} from "./Services/TodoService"
+var http_1 = require('angular2/http');
+var router_1 = require('angular2/router');
+var TodoApp = (function () {
+    function TodoApp() {
+        this.jozo = 0;
+    }
+    TodoApp = __decorate([
+        core_1.Component({
+            selector: 'establishment-details'
+        }),
+        core_1.View({
+            template: "\n        <div class=\"well\">\n            <h4>Additional information:</h4>\n            <div class=\"finedine-text\">\n                Category: @Html.DisplayFor(model => model.Category.Name)\n                <br />\n                Adress: @Html.DisplayFor(model => model.Address)\n                <br />\n                City: @Html.DisplayFor(model => model.Location.PostCode) @Html.DisplayFor(model => model.Location.City)\n                <br/>\n                Phone number: @Html.DisplayFor(model => model.PhoneNumber)\n                <br/>\n                Owner: @Html.DisplayFor(model => model.Owner.UserName)\n                <br />\n                Working hours: @Html.DisplayFor(model => model.WorkingHours)\n                <div class=\"center-align\" style=\"padding-top:5px;\">\n                    <br/>\n                    <span class=\"glyphicon glyphicon-star\"></span>\n                    <span class=\"glyphicon glyphicon-star\"></span>\n                    <span class=\"glyphicon glyphicon-star\"></span>\n                    <span class=\"glyphicon glyphicon-star\"></span>\n                    <span class=\"glyphicon glyphicon-star-empty\"></span>\n                    <p class=\"main-rating\"> @Html.DisplayFor(model => model.MainRating) / 5.0 </p>\n                </div>\n            </div>\n        </div>\n"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TodoApp);
+    return TodoApp;
+})();
+exports.TodoApp = TodoApp;
+browser_1.bootstrap(TodoApp, [router_1.ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS]);
 //# sourceMappingURL=app.js.map
--- FineDine/Scripts/App/app.ts
+++ FineDine/Scripts/App/app.ts
@@ -1,1 +1,51 @@
-﻿
+﻿import {Component, View} from "angular2/core"
+import {bootstrap}    from 'angular2/platform/browser'
+
+//import {TodoItem} from "./Model/TodoItem"
+//import {TodoInput} from "./Components/TodoInput"
+//import {TodoProgress} from "./Components/TodoProgress"
+//import {TodoList} from "./Components/TodoList"
+//import {TodoService} from "./Services/TodoService"
+
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+
+@Component({
+    selector: 'establishment-details'
+                
+})
+@View({
+        template: `
+        <div class="well">
+            <h4>Additional information:</h4>
+            <div class="finedine-text">
+                Category: @Html.DisplayFor(model => model.Category.Name)
+                <br />
+                Adress: @Html.DisplayFor(model => model.Address)
+                <br />
+                City: @Html.DisplayFor(model => model.Location.PostCode) @Html.DisplayFor(model => model.Location.City)
+                <br/>
+                Phone number: @Html.DisplayFor(model => model.PhoneNumber)
+                <br/>
+                Owner: @Html.DisplayFor(model => model.Owner.UserName)
+                <br />
+                Working hours: @Html.DisplayFor(model => model.WorkingHours)
+                <div class="center-align" style="padding-top:5px;">
+                    <br/>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star-empty"></span>
+                    <p class="main-rating"> @Html.DisplayFor(model => model.MainRating) / 5.0 </p>
+                </div>
+            </div>
+        </div>
+`
+})
+export class TodoApp {
+    private jozo: number = 0;
+}
+
+bootstrap(TodoApp, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+
